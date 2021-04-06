6ab3fc3c9dd796b0bc86ae6696f82d8b92716d52
Linya-IronMan <cdthanye32@163.com>
2021-04-06 13:44:37 +0800

    feature/番茄todo

--- tomato-test/main.js
+++ tomato-test/main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow, ipcMain, Notification } = require('electron');
-const { Promise } = require('globalthis/implementation');
+const { app, BrowserWindow, ipcMain, Notification } = require('electron'); // 作用？
+// const { Promise } = require('globalthis/implementation');
 const { resolve } = require('path');
 
 let win; // 设置一个全局变量存储 BrowserWindow 实例，否则会被垃圾回收掉
@@ -9,30 +9,35 @@ app.on('ready', () => {
         width: 300,
         height: 300,
         webPreferences: {
-            nodeIntegration: true // 新版本中默认将node禁用，但是这里会用到本地文件，所以打开
+            nodeIntegration: true // 新版本中默认将node禁用（为了安全的考虑），但是这里会用到本地文件，所以打开
         }
     })
 
-    win.loadFile('./index.html')
+    win.loadFile('./index.html') // loadURL 加载远程页面
     setTimeout(handleIPC, 500)
 })
 
 function handleIPC() {
     win.webContents.send('do-some-render-work');
 
-    ipcMain.handle('work-notification', async () => {
+    ipcMain.handle('work-notification', async function() {
         let res = await new Promise((resolve, reject) => {
             let notification = new Notification({
                 title: '任务结束',
                 body: '是否开始休息',
-                actions: [{ text: '开始休息', type: 'button' }],
-                closeButtonText: '继续工作'
+                actions: [{ text: '开始休息', type: 'button' }], // 这段在windows 下没有什么作用
+                closeButtonText: '继续工作' // 这个也是 只在 MacOS 下有可用。
             })
             notification.show()
-            notification.on('action', () => {
+            notification.on('action', () => { // 点击 无用
                 resolve('rest')
             })
-            notification.on('close', () => {
+            notification.on('click', () => {
+                console.log('click happen')
+                resolve('rest')
+            })
+            notification.on('close', () => { // 点击了关闭按钮
+                console.log('close happen')
                 resolve('work')
             })
         })
--- tomato-test/renderer.js
+++ tomato-test/renderer.js
@@ -1,28 +1,36 @@
-const {ipcRenderer} = require('electron')
-const { Notification, document, setTimeout, alert } = require('globalthis/implementation')
-// timer.js 
+const {ipcRenderer} = require('electron') // 用于进程间通信
 const Timer = require('timer.js')
 
+// const { Notification, document, setTimeout, alert } = require('globalthis/implementation')
+
+
 function startWork() {
-    new Timer({
-        ontick: () => {
-            updateTimer()
+    let workTimer = new Timer({
+        ontick: (data) => {
+            updateTime(data)
         },
         onend: () => {
-            Notification()
+            notification()
         }
     })
-    workTimer.start(10)
+    workTimer.start(5)
 }
 
 function updateTime(ms) {
     let timerContainer = document.getElementById('timer-container')
-    timerContainer.innerText = ms;
+    console.log(ms, '========')
+    let s = (ms / 1000).toFixed(0)
+    let ss = s % 60
+    let mm = (s / 60).toFixed(0)
+
+    timerContainer.innerText = `${mm.toString().padStart(2,0)} : ${ss.toString().padStart(2,0)}`;
 }
 
 async function notification() {
+    
     let res = await ipcRenderer.invoke('work-notification')
     if (res === 'rest') {
+        console.log('rest !!!!')
         setTimeout(() => {
             alert('休息')
         }, 5000)
